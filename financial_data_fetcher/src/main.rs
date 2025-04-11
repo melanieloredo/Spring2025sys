a4dbc465477ae030c509179303f691a0d9b5a3d4
melanieloredo <156953241+melanieloredo@users.noreply.github.com>
2025-04-11 21:29:42 +0000

    updated structs & traits to include name for file output

--- financial_data_fetcher/src/main.rs
+++ financial_data_fetcher/src/main.rs
@@ -4,23 +4,40 @@ use serde::Deserialize;
 use std::{fs::File, io::{self, Write}, thread, time::Duration};
 use chrono::Utc;
 
+
+//to Deserialize API Response
+#[derive(Debug, Deserialize)]
+struct CoinGeckoResponse {
+    bitcoin: Option<CoinPrice>,
+    ethereum: Option<CoinPrice>,
+}
+
+#[derive(Debug, Deserialize)]
+struct CoinPrice {
+    usd: f64,
+}
+
 //Bitcoin, Ethereum and SP500 struct
 struct Bitcoin{
     pricing: f64,
+    name: String,
 }
 
 struct Ethereum{
     pricing: f64,
+    name: String,
 }
 
 struct SP500{
     pricing: f64,
+    name: String,
 }
 
 //trait pricing
 trait Pricing {
     fn get_price(&self) -> f64;
     fn save_to_file(&self, filename: &str);
+    fn get_name(&self) -> &str;
     fn get_price(&self) -> f64;
 }
 
